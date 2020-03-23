b5487648e951bc9b06a8e981a5e3eaa30bec997d
UVSQ-21807940 <37400158+UVSQ-21807940@users.noreply.github.com>
2020-03-23 23:24:02 +0100

    Create Direction.java

---
+++ src/main/java/uvsq21807940/Direction.java
@@ -0,0 +1,48 @@
+
+
+public class Direction {
+public float i;
+
+public Direction(int i) {
+	super();
+	this.i = i;
+}
+
+
+
+
+
+
+public void setI(float x) {
+i=x ;		
+}
+
+public float getI() {
+return i;
+}
+
+
+
+ 
+	static Direction EAST() {
+		return EAST();
+	}
+	static void SOUTHEAST() {
+	}
+	static void SOUTEAST() {
+	}
+	static void SOUTH() {
+	}
+	static void SOUTHWEST() {
+	}
+	static void WEST() {
+	}
+	static void NORTHWEST() {
+	}
+	static Direction NORTH() {
+		return NORTH();
+	}
+	static void NORTHEAST() {
+	}
+
+}
